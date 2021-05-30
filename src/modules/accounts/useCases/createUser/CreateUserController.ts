2a80fc62b7a70966faeba5e99e1238543706aed7
leopmrj <leopmrj@gmail.com>
2021-05-30 20:23:41 -0300

    password encrypted and check if email is already in use

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -7,13 +7,17 @@ class CreateUserController {
 	async handle(request: Request, response: Response): Promise<Response> {
 		const { name, email, password, driver_license } = request.body;
 		const createUserUseCase = container.resolve(CreateUserUseCase);
-		await createUserUseCase.execute({
-			name,
-			email,
-			password,
-			driver_license,
-		});
-		return response.status(201).send();
+		try {
+			await createUserUseCase.execute({
+				name,
+				email,
+				password,
+				driver_license,
+			});
+			return response.status(201).send();
+		} catch (error) {
+			return response.status(400).json({ error: error.message });
+		}
 	}
 }
 
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
+import { SimpleConsoleLogger } from "typeorm";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -16,10 +18,17 @@ class CreateUserUseCase {
 		password,
 		driver_license,
 	}: ICreateUserDTO): Promise<void> {
+		const userAlreadyExists = await this.usersRepository.findByEmail(email);
+		if (userAlreadyExists) {
+			throw new Error(`Email ${email} is already in use.`);
+		}
+
+		const passwordHash = await hash(password, 8);
+
 		await this.usersRepository.create({
 			name,
 			email,
-			password,
+			password: passwordHash,
 			driver_license,
 		});
 	}
